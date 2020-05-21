f209502a47d7e2c5588a690a94c73f277f626343
Pragya Srivastava <s.pragya2802@gmail.com>
2020-05-22 02:02:48 +0530

    jQuery form & Create API View

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet
 class  TweetModelSerializer(serializers.ModelSerializer):
-	user = UserDisplaySerializer()
+	user = UserDisplaySerializer(read_only=True)
 	class Meta:
 		model = Tweet
 		fields = [
--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from .views import (
+	TweetCreateAPIView,
 	TweetListAPIView
 	)
 
 urlpatterns = [
 	 	# url(r'^$',RedirectView.as_view(url="/")),
 	     url(r'^$',TweetListAPIView.as_view(), name='list'),
-		 #    url(r'^create/$',TweetCreateView.as_view(), name='create'),
+		 url(r'^create/$',TweetCreateAPIView.as_view(), name='create'),
 	 	#    url(r'^(?P<pk>\d+)/$',TweetDetailView.as_view(), name='detail'),
 	 	#    url(r'^(?P<pk>\d+)/update/$',TweetUpdateView.as_view(), name='update'),
 		 #    url(r'^(?P<pk>\d+)/delete/$',TweetDeleteView.as_view(), name='delete'),
