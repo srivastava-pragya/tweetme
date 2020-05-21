16fe41e9bfa1de0adc4b437ef1038520905c7ac8
Pragya Srivastava <s.pragya2802@gmail.com>
2020-05-22 00:23:14 +0530

    AJAX list view

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet
 class  TweetModelSerializer(serializers.ModelSerializer):
+	user = UserDisplaySerializer()
 	class Meta:
 		model = Tweet
 		fields = [
