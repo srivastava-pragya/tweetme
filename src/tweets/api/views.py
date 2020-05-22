7d6b6ddc6348459061f3e153f8de533f7aea47b3
Pragya Srivastava <s.pragya2802@gmail.com>
2020-05-23 00:49:17 +0530

    Pagination & Ajax load more option

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from rest_framework import generics
 from rest_framework import permissions
 from tweets.models import Tweet
+from .pagination import StandardResultsPagination
 from .serializers import TweetModelSerializer
 
 class TweetCreateAPIView(generics.CreateAPIView):
@@ -13,6 +14,7 @@ class TweetCreateAPIView(generics.CreateAPIView):
 
 class TweetListAPIView(generics.ListAPIView):
 	serializer_class = TweetModelSerializer
+	pagination_class = StandardResultsPagination
 
 	def get_queryset(self, *args, **kwargs):
 		qs = Tweet.objects.all().order_by("-timestamp")
